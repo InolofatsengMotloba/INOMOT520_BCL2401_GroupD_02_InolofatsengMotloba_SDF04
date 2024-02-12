2ca3a732d5558c670554c2d75a7bf27d436d2053
InolofatsengMotloba <156679504+InolofatsengMotloba@users.noreply.github.com>
2024-02-12 13:24:08 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# INOMOT520_BCL2024_GroupD_02_InolofatsengMotloba_SDF04
+https://scrimba.com/scrim/cbmPkNsd
---
+++ index.js
@@ -0,0 +1,72 @@
+let player = {
+    name: "Inolofatseng",
+    chips: 145
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": R" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got BlackJack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()   
+    }
+}
+
+// I was able to define arrays + reasign them + push to the array
+// I was able to create the "player" object with key values
+// I used comparison operators to define the condions that make the if else statements (conditionals) work
+// I used for loops to loop through the cards array and render out each of the cards
+// WHAT I FOUND DIFFICULT: Using the For loop and The Math objects
+//                         Using logical operators &  return statements
